fcdbad1d3281e72448c1fb400a6ef2fa99af6761
Justine Taningco <htaningco@gmail.com>
2024-03-01 11:54:58 +0800

    added the handlePhoneChange function whenever you type something in the phone input

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "345-434" },
+  ]);
+  const [newName, setNewName] = useState("nbnbv");
+  const [newPhone, setNewPhone] = useState("54543");
+  const handleNameChange = (e) => {
+    console.log(e.target.value);
+    console.log("line 10", e);
+    setNewName(e.target.value);
+  };
+  const handlePhoneChange = (e) => {
+    setNewPhone(e.target.value);
+  };
+  const addName = (e) => {
+    console.log("line 14 inside addName function", newName);
+    e.preventDefault();
+    const found = persons.some((person) => person.name == newName);
+    if (found) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    setPersons([...persons, { name: newName, number: newPhone }]);
+    setNewName("");
+  };
   return (
-    <>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:{" "}
+          <input onChange={handleNameChange} value={newName} name="name" />
+          <br></br>
+          number:{" "}
+          <input onChange={handlePhoneChange} value={newPhone} name="number" />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {persons.map((person, id) => (
+          <div key={id}>
+            <span>{person.name}</span>
+            <span>{person.number}</span>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
